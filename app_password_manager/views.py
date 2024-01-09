e48911733721e0d06b9536fc282a0de048fc811d
Audrey <116880046+richardaud@users.noreply.github.com>
2024-01-09 12:42:25 +0100

    Ajout d'une views formulaire .py

--- app_password_manager/views.py
+++ app_password_manager/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import SiteForm
+
+
+def ajouter_site(request):
+    if request.method == 'POST':
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            site = form.save(commit=False)
+            site.user = request.user  # Associer le site à l'utilisateur connecté
+            site.save()
+            return redirect('nom_de_la_vue_pour_afficher_le_carnet_adresses')
+    else:
+        form = SiteForm()
+
+    return render(request, 'forms.html', {'form': form})
