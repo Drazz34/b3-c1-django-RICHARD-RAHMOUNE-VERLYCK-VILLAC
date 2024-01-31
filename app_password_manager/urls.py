ceb23b8ecee842da03423f06141d6985ad28dbaa
Audrey <116880046+richardaud@users.noreply.github.com>
2024-01-31 16:31:12 +0100

    suppression site terminé

--- app_password_manager/urls.py
+++ app_password_manager/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'^delete/(?P<site_id>[0-9]+)/$', views.supprimer_site, name='supprimer_site')
 ]
--- app_password_manager/views.py
+++ app_password_manager/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Site
 
+
 def index(request):
+    sites = Site.objects.all()
+    return render(request, 'index.html', {'sites': sites})
+
+
+def supprimer_site(request, site_id=None):
+    if site_id:
+        site = Site.objects.get(pk=int(site_id))
+        site.delete()
     sites = Site.objects.all()
     return render(request, 'index.html', {'sites': sites})
