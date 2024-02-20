a35fe9f2baa9f2905e2f92dfb8c7dbb3a05ae6f0
Audrey <116880046+richardaud@users.noreply.github.com>
2024-02-20 17:16:20 +0100

    ImportExport

--- app_password_manager/urls.py
+++ app_password_manager/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ajouter_site', views.ajouter_site, name="ajouter_site"),
-    path(r'^delete/(?P<site_id>[0-9]+)/$', views.supprimer_site, name='supprimer_site')
+    path(r'^delete/(?P<site_id>[0-9]+)/$', views.supprimer_site, name='supprimer_site'),
+    path('export_passwords/', views.export_passwords, name='export_passwords'),
+    path('import_csv/', views.import_csv, name='import_csv'),
 ]
--- app_password_manager/views.py
+++ app_password_manager/views.py
@@ -1,3 +1,6 @@
+
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Site
 from .forms import SiteForm
@@ -25,3 +28,35 @@ def supprimer_site(request, site_id=None):
         site = Site.objects.get(pk=int(site_id))
         site.delete()
     return redirect('index')
+
+def export_passwords(request):
+    sites = Site.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="passwords.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Nom', 'URL', 'Identifiant', 'Mot de passe'])
+    for site in sites:
+        writer.writerow([site.id, site.nom, site.url, site.identifiant, site.mot_de_passe])
+
+    return response
+
+def import_csv(request):
+    if request.method == 'POST' and request.FILES.get('csv_file'):
+        csv_file = request.FILES['csv_file']
+        if csv_file.name.endswith('.csv'):
+
+            decoded_file = csv_file.read().decode('utf-8').splitlines()
+            csv_reader = csv.reader(decoded_file)
+            for row in csv_reader:
+
+                _, created = Site.objects.get_or_create(
+                    nom=row[1],
+                    url=row[2],
+                    identifiant=row[3],
+                    mot_de_passe=row[4]
+                )
+            return HttpResponse("CSV file imported successfully.")
+        else:
+            return HttpResponse("File is not a CSV.")
+    return render(request, 'index.html')
